b7327984ad9e1ef1983dea4d4b4acda426cc3878
Ratish149 <ratish.shakya149@gmail.com>
2026-09-02 15:01:14 +0545

    chore: configure database settings to use environment variables with PostgreSQL default

--- calleatmandu/settings.py
+++ calleatmandu/settings.py
@@ -155,10 +155,21 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    }
+} """
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.postgresql"),
+        "NAME": os.getenv("DB_NAME", ""),
+        "USER": os.getenv("DB_USER", ""),
+        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", ""),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
